7d8acfa17a32eb4102a64eb370e85af3ac2d8ca3
Chasten <chasten.rippon@gmail.com>
2025-02-06 21:03:47 -0500

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fishButton = document.getElementById('fish-button');
+
+    fishButton.addEventListener('click', async () => {
+        window.location.href = 'index.html';
+    });
+});
